f78fd988ede3d0db380536127acf9c9903737562
Pinak <pinak@git.com>
2026-08-10 19:43:44 +0530

    Implemented free hosting spin-up alert

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,70 +13,88 @@ import ForgotPassword from "../forgotPassword/ForgotPassword";
 import SearchBar from "../searchbar/SearchBar";
 
 export default function Header() {
-  const navRef = useRef();
-  const loginRef = useRef();
-  const signUpRef = useRef();
-  const forgotPasswordRef = useRef();
-  const navigate = useNavigate();
+    const navRef = useRef();
+    const loginRef = useRef();
+    const signUpRef = useRef();
+    const forgotPasswordRef = useRef();
+    const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.user);
-  const { cartItemsCount } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+    const { isAuthenticated, isLoading } = useSelector((state) => state.user);
+    const { cartItemsCount } = useSelector((state) => state.cart);
 
-  function openNav() {
-    navRef.current.classList.add("navbar--active");
-  }
-
-  function openLogin() {
-    if (isAuthenticated) {
-      return navigate("/profile", { replace: true });
+    function openNav() {
+        navRef.current.classList.add("navbar--active");
     }
-    dispatch(clearLoadingAndValidationError());
-    loginRef.current.classList.add("login--active");
-  }
 
-  return (
-    <div className="header">
-      <Link to="/" className="header__logoLink">
-        <h1 className="header__logo">Shoppp It On</h1>
-      </Link>
+    function openLogin() {
+        if (isAuthenticated) {
+            return navigate("/profile", { replace: true });
+        }
+        dispatch(clearLoadingAndValidationError());
+        loginRef.current.classList.add("login--active");
+    }
 
-      <Navbar navRef={navRef} />
+    return (
+        <>
+            <div className="header">
+                <Link to="/" className="header__logoLink">
+                    <h1 className="header__logo">Shoppp It On</h1>
+                </Link>
 
-      <div className="header__actions">
-        <div className="header__hamburgerButton" onClick={openNav}>
-          <div className="header__hamburgerSlice"></div>
-          <div className="header__hamburgerSlice"></div>
-          <div className="header__hamburgerSlice"></div>
-        </div>
+                <Navbar navRef={navRef} />
 
-        <SearchBar />
+                <div className="header__actions">
+                    <div className="header__hamburgerButton" onClick={openNav}>
+                        <div className="header__hamburgerSlice"></div>
+                        <div className="header__hamburgerSlice"></div>
+                        <div className="header__hamburgerSlice"></div>
+                    </div>
 
-        <div className="header__cartAndProfile">
-          <div className="header__cart" onClick={() => navigate("/cart")}>
-            <FaShoppingCart /> CART
-            {cartItemsCount != null ? (
-              <sup className="header__cartItemsCount">{cartItemsCount}</sup>
-            ) : (
-              ""
+                    <SearchBar />
+
+                    <div className="header__cartAndProfile">
+                        <div
+                            className="header__cart"
+                            onClick={() => navigate("/cart")}
+                        >
+                            <FaShoppingCart /> CART
+                            {cartItemsCount != null ? (
+                                <sup className="header__cartItemsCount">
+                                    {cartItemsCount}
+                                </sup>
+                            ) : (
+                                ""
+                            )}
+                        </div>
+                        <div className="header__profile" onClick={openLogin}>
+                            <FaUser /> PROFILE
+                        </div>
+                    </div>
+                </div>
+
+                <Login
+                    loginRef={loginRef}
+                    signUpRef={signUpRef}
+                    forgotPasswordRef={forgotPasswordRef}
+                />
+                <SignUp signUpRef={signUpRef} loginRef={loginRef} />
+                <ForgotPassword
+                    forgotPasswordRef={forgotPasswordRef}
+                    loginRef={loginRef}
+                />
+            </div>
+
+            {isLoading && (
+                <div className="free-hosting-alert">
+                    <span className="free-hosting-alert-icon">ℹ️</span>
+                    <p className="free-hosting-alert-text">
+                        <strong>Note:</strong> First load may take 5-10 seconds
+                        due to free hosting spin-up. Thank you for your
+                        patience!
+                    </p>
+                </div>
             )}
-          </div>
-          <div className="header__profile" onClick={openLogin}>
-            <FaUser /> PROFILE
-          </div>
-        </div>
-      </div>
-
-      <Login
-        loginRef={loginRef}
-        signUpRef={signUpRef}
-        forgotPasswordRef={forgotPasswordRef}
-      />
-      <SignUp signUpRef={signUpRef} loginRef={loginRef} />
-      <ForgotPassword
-        forgotPasswordRef={forgotPasswordRef}
-        loginRef={loginRef}
-      />
-    </div>
-  );
+        </>
+    );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,28 +3,39 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductsThunk } from "../../redux/slices/productSlice";
 import CategorySection from "../categorySection/CategorySection";
+import Loading from "../loading/Loading";
 
 export default function Home() {
-  const { latestProducts } = useSelector((state) => state.product);
+    const { latestProducts, isLoading } = useSelector((state) => state.product);
+    const { isLoading: isAuthLoading } = useSelector((state) => state.user);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getProductsThunk("latest=true"));
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getProductsThunk("latest=true"));
+    }, [dispatch]);
 
-  return (
-    <div className="home">
-      <div className="home__hero">
-        <div className="home__heroText">
-          <div>GET READY FOR</div>
-          <div>YOUR FAVOURITE SHOPPING</div>
+    return (
+        <div className="home">
+            <div className="home__hero">
+                <div className="home__heroText">
+                    <div>GET READY FOR</div>
+                    <div>YOUR FAVOURITE SHOPPING</div>
+                </div>
+            </div>
+
+            {isLoading || isAuthLoading ? (
+                <Loading />
+            ) : (
+                latestProducts?.map((categoryData, index) => {
+                    return (
+                        <CategorySection
+                            key={index}
+                            categoryData={categoryData}
+                        />
+                    );
+                })
+            )}
         </div>
-      </div>
-
-      {latestProducts?.map((categoryData, index) => {
-        return <CategorySection key={index} categoryData={categoryData} />;
-      })}
-    </div>
-  );
+    );
 }
